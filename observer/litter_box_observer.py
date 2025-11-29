f883c0465f7077745fee76ee634a46ce209f2c6d
zappyfish <liamkelly2015@gmail.com>
2025-11-29 03:06:56 -0600

    wip

--- observer/litter_box_observer.py
+++ observer/litter_box_observer.py
@@ -1,4 +1,16 @@
+from observer.usage_observer import UsageObserver
 
 class LitterBoxObserver:
 
-    def __init__
+    def __init__(
+        self,
+        observer_config: observer_config_pb2.LitterBoxObserverConfig)
+
+    def begin_observing(self):
+        callback = functools.partial(LitterBoxObserver.begin_observing, self)
+        self._usage_observer.begin_observing(callback)
+
+    def on_litterbox_used(self, usage_metadata):
+        # Grab video buffer
+        # Store video
+        # Store metadata
---
+++ observer/usage_observer.py
@@ -0,0 +1,8 @@
+
+class UsageObserver:
+
+    def __init__():
+        pass
+
+    def begin_observing(self, on_usage_observed_callback: Callable):
+        pass
